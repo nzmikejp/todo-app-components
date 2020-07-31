6d331d380bafaf8c7b4ffebf49a14f03b6ac4c40
Mike Jackson-Poter <nzmikejp@gmail.com>
2020-07-31 16:26:15 +1200

    pie day friday done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import NewToDoForm from './NewToDoForm'
+import ToDo from './ToDo'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+        todos: [
+          {
+            id: 1,
+            content: 'Water Plants',
+            priority: 'Urgent'  
+          },
+          {
+            id: 2,
+            content: 'Cook Dinner',
+            priority: 'Important'  
+          },
+          {
+            id: 3,
+            content: 'Watch Youtube',
+            priority: 'Important'  
+          }, 
+        ]
+    }
+  }
+
+  addTodo = (data) => {
+    var newTodo = {
+      id: Date.now(),
+      ...data
+    }
+
+    var newList = [newTodo, ...this.state.todos]
+
+    this.setState({
+      todos: newList
+    })
+  }
+
+  removeTodo = () => {}
+  updateTodo = () => {}
+  
+  render(){
+    return (
+      <div className="wrap">
+        <div className="container">
+          <div className="todos">
+            {
+              this.state.todos.map((todo)=>{
+
+                var todoProps = {
+                  key: todo.id,
+                  ...todo
+                }
+
+                return (
+                  <ToDo {...todoProps} />
+
+                )
+              })
+            }
+
+            <NewToDoForm addTodo={this.addTodo}/>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/NewToDoForm.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+
+class NewToDoForm extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            contentInput: '',
+            priorityInput: ''
+        }
+    }
+
+    handleContentChange = (e) => {
+        this.setState({
+            contentInput: e.target.value
+        })
+    }
+    
+    handlePriortyChange = (e) => {
+        this.setState({
+            priorityInput: e.target.value
+        })
+    }
+
+    handleAdd = (e) => {
+        e.preventDefault()
+        var data = {
+            content: this.state.contentInput,
+            priority: this.state.priorityInput
+        }
+
+        this.props.addTodo(data)
+    }
+
+    render(){
+        return (
+            <div className="todo new-todo">
+              <form className="todo-body">
+                  <div className="form-group">
+                    <label htmlFor="content-input">Content</label>
+                    <input type="text" className="form-control" id="content-input" onChange={this.handleContentChange}/>
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="priority-input">Priority</label>
+                    <input type="text" className="form-control" id="priority-input" onChange={this.handlePriortyChange}/>
+                  </div>
+                  <button type="submit" className="btn btn-primary" onClick={this.handleAdd}>Add</button>
+              </form>
+            </div>
+        )
+    }
+}
+
+export default NewToDoForm
